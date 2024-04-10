0738780f6a9a98467984d2f7b3138728e3eca63d
estudiantede <99504012+estudiantede@users.noreply.github.com>
2024-04-10 17:28:18 -0300

    Guìa 3 terminada

--- haskell.hs
+++ haskell.hs
@@ -1,3 +1,5 @@
+-- PRÁCTICA 3
+
 --Ej 1
 
 f :: Int -> Int
@@ -86,14 +88,134 @@ digitoUnidades x = mod x 10
 digitoDecenas :: Int -> Int
 digitoDecenas x = mod (div x 10) 10  
 
+-----------------------------------------------------------------------------------------------------------------
+--Ej 3)
 
+estanRelacionados :: Integer -> Integer -> Bool
+estanRelacionados x y = mod x y == 0
+-----------------------------------------------------------------------------------------------------------------
+--4) 
 
+--a)
+prodInterno :: (Int, Int) -> (Int, Int) -> Int
+prodInterno x y = (fst x) * (fst y) + (snd x) * (snd y)
 
+--b)
+todoMenor :: (Int, Int) -> (Int, Int) -> Bool
+todoMenor x y = (fst x) < (fst y) && (snd x) < (snd y)
 
+--c)
+distanciaPuntos :: (Float, Float) -> (Float, Float)  -> Float
+distanciaPuntos x y = sqrt ( ((fst x) - fst (y))  ^ 2 + ((snd x) - snd (y))  ^ 2)
 
+--d)
+thrd:: (Int,Int,Int) -> Int
+thrd (_,_,c) = c
 
+sumaTerna :: (Int, Int, Int) -> Int
+sumaTerna (x, y, z) = x + y + z
 
+--e)
+sumarSoloMultiplos :: (Int, Int, Int) -> Int -> Int
+sumarSoloMultiplos(x, y, z) a | ((mod x a) == 0) && ((mod y a) == 0) && ((mod z a) == 0) = x + y + z
+                              | ((mod x a) == 0) && ((mod y a)) == 0 = x + y
+                              | ((mod x a) == 0) && ((mod z a)) == 0 = x + z
+                              | ((mod x a) == 0) = x
+                              | ((mod y a) == 0) && (mod z a) == 0 = y + z
+                              | ((mod z a) == 0) = z
+                              | ((mod y a) == 0) = y
+                              | otherwise = 0
 
+--f)
+posPrimerPar :: (Int, Int, Int) -> Int
+posPrimerPar (x, y, z) | (mod x 2) == 0 = 0
+                       | (mod y 2) == 0 = 0
+                       | (mod z 2) == 0 = 0
+                       | (mod x 2) == 1 && (mod y 2) /= 0 && (mod z 2) /= 0 = 4
+
+--g) 
+crearPar :: a -> b -> (a, b)
+crearPar x y = (x, y)
+
+--h) 
+invertir :: (a, b) -> (b, a)
+invertir (x, y) = (y, x)
+
+-----------------------------------------------------------------------------------------------------------------
+--Ej 5
+todosMenores :: (Int, Int, Int) -> Bool
+todosMenores (a, b, c) = 2
+
+f :: Int -> Int
+f x | x <= 7 = x ^ 2 
+    | x > 7 = 2n - 1
+
+g:: Int -> Int
+g x | mod x 2 == 0 = div x 2
+    | otherwise = 3 * n + 1
+-----------------------------------------------------------------------------------------------------------------
+--Ej 6
+
+bisiesto :: Int -> Bool
+bisiesto x | (mod x 4 /= 0) = False
+           | (mod x 100 == 0 && mod x 400 /= 0) = False
+           | otherwise = True
+
+-----------------------------------------------------------------------------------------------------------------
+--Ej 7
+distanciaManhattan :: (Float, Float, Float) -> (Float, Float, Float) -> Float
+distanciaManhattan (a, b, c) (x, y ,z) = abs (a - x) + abs (b - y) + abs (c - z)
+
+-----------------------------------------------------------------------------------------------------------------
+--Ej 8
+comparar :: Int -> Int -> Int
+comparar x y | sumaUltimosDosDigitos(x) < sumaUltimosDosDigitos(y) = 1
+             | sumaUltimosDosDigitos(x) = sumaUltimosDosDigitos(y) = 0
+             | otherwise = -1
+
+sumaUltimosDosDigitos :: Int -> Int
+sumaUltimosDosDigitos x = mod (abs x) 10 + mod (div (abs x) 10) 10 
+
+-----------------------------------------------------------------------------------------------------------------
+--Ej 9
+{-
+problema f1 (x: R) : Float {
+    requiere: {true}
+    asegura: {res devuelve 1 si x es 0. En caso contrario devuelve 0}
+}
+
+problema f2 (x: R) : R {
+    requiere: {(x == 1) || (x == -1)}
+    asegura: {res = 15 <--> x == 1}
+    asegura: {res = -15 <--> x == -1}
+}
+
+problema f3 (x: R): R {
+    requiere: {true}
+    asegura: {res = 7 <--> n <= 9. En caso contrario, verificar si x >= 3 <--> res = 5}
+}
+
+problema f4 (x, y: R) : R {
+    requiere: {true}
+    asegura: {res = (x+y)/2}
+}
+
+problema f5 (x: (R, R)) : R {
+    requiere: {true}
+    asegura: {res es la division entre la suma de la primera coordenada y la segunda coordenada y 2 }
+}
+
+problema f6 (x: R, y: Z) : Bool {
+    requiere: {true}
+    asegura: {res = True <--> truncate (x) == b}
+}
+
+problema truncate (x: R) : Z {
+    requiere: {true}
+    asegura: {res = al nùmero entero màs cercano de x a 0}
+}
+
+-}
 
 
 
@@ -142,8 +264,7 @@ Eq t :: t -> t
 crearPar :: t -> t -> (t, t)
 crearPar x y = (x, y)
 
---todosMenores :: (Int, Int, Int) -> Bool
---todosMenores (a, b, c) = 2
+--
 
 f :: Int -> Int
 f x | x <= 7 = x ^ 2
@@ -153,21 +274,6 @@ g :: Int -> Int
 g x | mod x 2 == 0 = div x 2
     | otherwise = 3 * x + 1
 
-bisiesto :: Int -> Bool
-bisiesto x | (mod x 4 /= 0) = False
-           | (mod x 100 == 0 && mod x 400 /= 0) = False
-           | otherwise = True
-
-distanciaManhattan :: (Float, Float, Float) -> (Float, Float, Float) -> Float
-distanciaManhattan (a, b, c) (x, y ,z) = abs (a - x) + abs (b - y) + abs (c - z)
-
-comparar :: Int -> Int -> Int
-comparar x y | sumaUltimosDosDigitos(x) < sumaUltimosDosDigitos(y) = 1
-             | sumaUltimosDosDigitos(x) = sumaUltimosDosDigitos(y) = 0
-             | otherwise = -1
-
-sumaUltimosDosDigitos :: Int -> Int
-sumaUltimosDosDigitos x = mod (abs x) 10 + mod (div (abs x) 10) 10 
 
 
 
@@ -175,40 +281,10 @@ sumaUltimosDosDigitos x = mod (abs x) 10 + mod (div (abs x) 10) 10
 
 
 
-problema f1 (x: R) : Float {
-    requiere: {true}
-    asegura: {res devuelve 1 si x es 0. En caso contrario devuelve 0}
-}
 
-problema f2 (x: R) : R {
-    requiere: {(x == 1) || (x == -1)}
-    asegura: {res = 15 <--> x == 1}
-    asegura: {res = -15 <--> x == -1}
-}
 
-problema f3 (x: R): R {
-    requiere: {true}
-    asegura: {res = 7 <--> n <= 9. En caso contrario, verificar si x >= 3 <--> res = 5}
-}
 
-problema f4 (x, y: R) : R {
-    requiere: {true}
-    asegura: {res = (x+y)/2}
-}
 
-problema f5 (x: (R, R)) : R {
-    requiere: {true}
-    asegura: {res es la division entre la suma de la primera coordenada y la segunda coordenada y 2 }
-}
 
-problema f6 (x: R, y: Z) : Bool {
-    requiere: {true}
-    asegura: {res = True <--> truncate (x) == b}
-}
-
-problema truncate (x: R) : Z {
-    requiere: {true}
-    asegura: {res = al nùmero entero màs cercano de x a 0}
-}
 
 
