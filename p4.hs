602a96c94be04329eedc794c5e6729666f32f69d
estudiantede <inakifrutos00@gmail.com>
2024-04-14 12:38:20 -0300

    Práctica 4. Ej 1

--- p4.hs
+++ p4.hs
@@ -1,3 +1,48 @@
 -- PRÁCTICA 4
 
 --Ej 1
+
+fibonacci :: Int -> Int
+fibonacci 0 = 0
+fibonacci 1 = 1
+fibonacci x = fibonacci (x - 1) + fibonacci (x - 2)
+
+--Ej 2
+
+--Ej 3
+
+--Ej 4
+
+--Ej 5
+
+--Ej 6
+
+--Ej 7
+
+--Ej 8
+
+--Ej 9
+
+--Ej 10
+
+--Ej 11
+
+--Ej 12
+
+--Ej 13
+
+--Ej 14
+
+--Ej 15
+
+--Ej 16
+
+--Ej 17
+
+--Ej 18
+
+--Ej 19
+
+--Ej 20
+
+--Ej 21
