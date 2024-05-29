db3142f7f5c3ddd7907c515f69bec0c1c6462cef
estudiantede <99504012+estudiantede@users.noreply.github.com>
2024-05-29 13:55:15 -0300

    Agregar numeros de ejercicios respectivo a cada uno

--- Prácticas/Soluciones/p8.py
+++ Prácticas/Soluciones/p8.py
@@ -1,9 +1,16 @@
+#Archivos
+
+#1
+    #1
 def contar_lineas(nombre_archivo: str) -> int:
     archivo = open(nombre_archivo, 'r')
     res = len(archivo.readlines())
     archivo.close()
     return res
+    #2
 
+    #3
+#2
 def clonar_sin_comentarios(nombre_archivo: str):
     archivo = open(nombre_archivo, 'r')
     
@@ -26,6 +33,9 @@ def es_un_comentario(s: str) -> bool:
         es_cometario = True
     return es_cometario
 
+
+
+#3
 def invertir_lineas(nombre_archivo: str):
     archivo_in = open(nombre_archivo, 'r')
     lista: list [str] = archivo_in.readlines()
@@ -37,11 +47,12 @@ def invertir_lineas(nombre_archivo: str):
     archivo.writelines(lista)
     archivo_out.close()
 
+#4
 def agregar_frase_al_final(nombre_archivo: str, frase: str):
     archivo = open(nombre_archivo, 'a')
     archivo.write(frase)
     archivo.close
-
+#5
 def agregar_frase_al_principio(nombre_archivo: str, frase: str):
     archivo = open (nombre_archivo, 'r')
     lista = archivo.readlines()
@@ -51,4 +62,47 @@ def agregar_frase_al_principio(nombre_archivo: str, frase: str):
     archivo = open(nombre_archivo, 'w')
     archivo.writelines(lista)
     archivo.close()
-    
+
+#6
+
+
+#7
+
+
+#Pilas
+
+#8
+
+#9
+
+#10
+
+#11
+
+#12
+
+#Colas
+
+#13
+
+#14
+
+#15
+
+#16
+
+#17
+
+#18
+
+#Diccionarios
+
+#19
+
+#20
+
+#21
+
+#22
+
+#23
